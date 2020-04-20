8d9af62a9de9f51d6213751ad5697b34aaa6dfe8
vindard <17693119+vindard@users.noreply.github.com>
2020-04-19 21:56:25 -0400

    create coin class to represent list of transfers with valid checker

--- pngcoin.py
+++ pngcoin.py
@@ -1,17 +1,47 @@
 from PIL import Image
 
-def validate():
-    """Just validate one transfer"""
-    user_input = input("Is this a valid signature? (Y/n): ")
-    if user_input.lower() == "y":
-        print("It's valid!")
-    else:
-        print("It's not valid!")
 
-def display_and_verify(filename):
-    img = Image.open(filename)
-    img.show()
-    validate()
+class PNGCoin:
+
+    def __init__(self, transfers):
+        self.transfers = transfers  # Instances of PIL.Image
+        self._valid = None
+
+    @staticmethod
+    def validate_txn(img):
+        img.show()
+
+        user_input = input("Is this a valid signature? (Y/n): ")
+        if user_input.lower() == "y":
+            return True
+        else:
+            return False
+
+    def validate_coin(self):
+        for txfr in self.transfers:
+            if not self.validate_txn(txfr):
+                return False
+
+        return True
+
+    @property
+    def valid(self):
+        if self._valid is None:
+            self._valid = self.validate_coin()
+
+        return self._valid
+
 
 if __name__ == "__main__":
-    display_and_verify("alice.png")
+    coin = PNGCoin([
+        Image.open("alice.png"),
+        Image.open("alice-to-bob.png"),
+    ])
+
+    bad_coin = PNGCoin([
+        Image.open("alice.png"),
+        Image.open("alice-to-bob-forged.png"),
+    ])
+
+    print(coin.valid)
+    print(bad_coin.valid)
